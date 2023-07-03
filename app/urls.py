44dcc10971467deb0d3a6a73476e80bc60839bfd
lamiiz <lamiizmuhammed.l4miiix@gmail.com>
2023-07-03 16:59:05 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path
+from . import views
+
+app_name = "app"
+
+urlpatterns = [
+     path("", views.IndexView.as_view(), name="index"),
+     path("chart", views.chartjsView.as_view(), name="chartjs"),
+     path("buttons", views.buttonsView.as_view(), name="buttons"),
+     path("typography", views.typographyView.as_view(), name="typography"),
+     path("icons", views.iconsView.as_view(), name="icons"),
+     path("forms", views.formsView.as_view(), name="forms"),
+     path("table", views.tableView.as_view(), name="tables"),
+     path("blankPage", views.blankPageView.as_view(), name="Blank_Page"),
+     path("404", views.error404View.as_view(), name="error404"),
+     path("500", views.error500View.as_view(), name="error500"),
+]
---
+++ app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.views.generic import ListView,TemplateView
+from . models import Book
+
+# Create your views here.
+
+class IndexView(TemplateView):
+    template_name='app/index.html'
+
+class chartjsView(TemplateView):
+    template_name='app/charts/chartjs.html'
+
+
+class buttonsView(TemplateView):
+    template_name='app/ui-features/buttons.html'
+    
+class typographyView(TemplateView):
+    template_name='app/ui-features/typography.html'
+
+class iconsView(TemplateView):
+    template_name='app/icons/mdi.html'
+
+class formsView(TemplateView):
+    template_name='app/forms/basic_elements.html'
+
+class tableView(TemplateView):
+    template_name='app/tables/basic-table.html'
+
+class blankPageView(TemplateView):
+    template_name='app/samples/blank-page.html'
+
+class error404View(TemplateView):
+    template_name='app/samples/error-404.html'
+
+
+class error500View(TemplateView):
+    template_name='app/samples/error-500.html'
